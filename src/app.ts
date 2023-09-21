e2a659ee4675330e85b057651911ccd60fa778aa
Andréas <andreasforsbom@gmail.com>
2023-09-21 23:50:22 +0200

    Added autojoin functionality. Added possibility to encrypt and decrypt messages in an encrypted room

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,80 @@
-import sdk, { RoomEvent, ClientEvent, ReceiptType } from "matrix-js-sdk";
-import { MATRIX_ACCESS_TOKEN, MATRIX_BASE_URL, MATRIX_USER_ID } from "./config";
+import sdk, {
+  RoomEvent,
+  ClientEvent,
+  ReceiptType,
+  RoomMemberEvent,
+  LocalStorageCryptoStore,
+} from "matrix-js-sdk";
+import LocalStorage from "node-localstorage";
+import {
+  MATRIX_ACCESS_TOKEN,
+  MATRIX_BASE_URL,
+  MATRIX_USER_ID,
+  MATRIX_DEVICE_ID,
+} from "./config";
 import { messageReceived } from "./messages";
+
+import Olm from "@matrix-org/olm";
+global.Olm = Olm;
+
+const localStorage = new LocalStorage.LocalStorage("./storage");
 
 export const client = sdk.createClient({
   baseUrl: MATRIX_BASE_URL,
   accessToken: MATRIX_ACCESS_TOKEN,
   userId: MATRIX_USER_ID,
+  deviceId: MATRIX_DEVICE_ID,
+  cryptoStore: new LocalStorageCryptoStore(localStorage),
 });
 
-// Sync Bot State
-client.once(ClientEvent.Sync, (state) => {
-  if (state === "PREPARED") {
-    // Message Event
-    client.on(RoomEvent.Timeline, async (event) => {
-      // If the event is not a message, return
-      if (event.getType() !== "m.room.message") return;
-      // If the message is from the bot, return
-      if (event.getSender() === client.getUserId()) return;
 
-      // Get the message content
-      const message = event.getContent().body;
-      const sender = event.getSender();
-      const room = event.getRoomId();
+const init = async () => {
+  await client.initCrypto();
 
-      // If the message is undefined, return
-      if (sender === undefined || room === undefined) return;
+  // Keep this for now, probably not a good idea in the long run
+  client.setGlobalErrorOnUnknownDevices(false);
 
-      // Mark the message as read
-      await client.sendReadReceipt(event, ReceiptType.Read, true);
+  // Sync Bot State
+  client.once(ClientEvent.Sync, (state) => {
+    if (state === "PREPARED") {
+      // Message Event
+      client.on(RoomEvent.Timeline, async (event) => {
+	if (event.getType() === "m.room.encrypted") {
+		await client.decryptEventIfNeeded(event);
+	}
 
-      messageReceived({ room, sender, message });
-    });
-  }
-});
+	// If the event is not a message, return
+        if (event.getType() !== "m.room.message") return;
 
-client.startClient();
+        // If the message is from the bot, return
+        if (event.getSender() === client.getUserId()) return;
+
+        // Get the message content
+        const message = event.getContent().body;
+        const sender = event.getSender();
+        const room = event.getRoomId();
+        // If the message is undefined, return
+        if (sender === undefined || room === undefined) return;
+
+        // Mark the message as read
+        await client.sendReadReceipt(event, ReceiptType.Read, true);
+
+        messageReceived({ room, sender, message });
+      });
+
+      // Automatically join rooms when invited
+      client.on(RoomMemberEvent.Membership, (event, member) => {
+        if (
+          member.membership === "invite" &&
+          member.userId === client.getUserId()
+        ) {
+          client.joinRoom(member.roomId);
+        }
+      });
+    }
+  });
+
+  client.startClient();
+};
+
+init();
--- src/config.ts
+++ src/config.ts
@@ -4,3 +4,4 @@ export const MATRIX_BASE_URL =
   process.env.MATRIX_BASE_URL ?? "https://matrix.org";
 export const MATRIX_ACCESS_TOKEN = process.env.MATRIX_ACCESS_TOKEN ?? "";
 export const MATRIX_USER_ID = process.env.MATRIX_USER_ID ?? "";
+export const MATRIX_DEVICE_ID = process.env.MATRIX_DEVICE_ID ?? "";
